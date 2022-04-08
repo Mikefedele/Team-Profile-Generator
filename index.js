07ce2cf5788a42d7e6c0a3ebf109fa10f188645b
Mike Fedele <mikefid11@gmail.com>
2022-04-07 22:17:30 -0400

    when inquirer to multi questions

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ inquirer
       type: 'list', 
       name: 'role',
       message: 'Role on the team?',
-      choices: ["Manager", "Engineer" ,"Employee"],
+      choices: ["Manager", "Engineer" ,"Employee", "Intern"],
     },
     {
       type: 'input',
@@ -39,10 +39,139 @@ inquirer
       type: 'input',
       message: 'email address?',
       name: 'email',
-    }
+    },
+    {
+      type: 'input',
+      message: "Enter office number",
+      name: 'officeNumb',
+      when: (response) => {
+        if (response.role === "Manager")
+        return true;
+      }
+    }, 
+    {
+      type: 'input',
+      message: "What is your Github username?",
+      name: 'gitName',
+      when: (response) => {
+        if (response.role === "Engineer")
+        return true;
+      }
+    },
+    {
+      type: 'input',
+      message: "Where do you go to school?",
+      name: 'school',
+      when: (response) => {
+        if (response.role === "Intern")
+        return true;
+      }
+    } 
+
+
+
   ]).then(response => console.log(response));
+
+// .then((response) => { 
+    
+//     switch (response.role) {
+//   case role[0]:
+//     inquirer
+//     .prompt([
+//       {
+//         type: 'input',
+//         message: "Enter office number",
+//         name: 'officeNumb',
+//       } 
+//     ])
+//     break;
+
+//   case role[1]:  
+//   inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: "What is your Github username?",
+//       name: 'gitName',
+//     } 
+//   ])
+//   break;
+
+//   case role[2]:
+//     inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: "Where do you go to school?",
+//       name: 'school',
+//     } 
+//   ])
+//     break;
+
+// }} 
+// ).then(response => console.log(response));
+  
+
   // .then(response => writeToFile("", response));
 
+// const roleSwitch = response.role
+
+// switch (response.role) {
+//   case Manager:
+//     inquirer
+//     .prompt([
+//       {
+//         type: 'input',
+//         message: "Enter office number",
+//         name: 'officeNumb',
+//       } 
+//     ])
+//     break;
+
+//   case Engineer:  
+//   inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       message: "What is your Github username?",
+//       name: 'gitName',
+//     } 
+//   ])
+//   break;
+
+//   case Intern:
+//     inquirer
+//   .prompt([
+    // {
+    //   type: 'input',
+    //   message: "Where do you go to school?",
+    //   name: 'school',
+    // } 
+//   ])
+//     break;
+
+// }
+
+
+//   if (response.role == Manager) {
+//     inquirer
+//       .prompt([
+//         {
+//           type: 'input',
+//           message: "Enter office number",
+//           name: 'officeNumb',
+//         } 
+
+//     ])
+//   } else if () {
+
+//   }
+
+// if (condition) {
+  
+// } else  {
+  
+// }
 
 
   //*THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
